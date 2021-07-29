777c9224ed0f89c1f658272ddad8961c3e1df5cc
Stanislav <S.tanislav-Z.abrodin@yandex.ru>
2021-07-29 18:13:09 +0400

    second

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,10 +2,15 @@ const link = document.getElementsByClassName("menu__link");
 const menu__item = document.getElementsByClassName('menu__item');
 for (let i = 0; i < link.length; i++) {
     link.item(i).onclick = () => {
-    	for (let j = 0; j < document.querySelectorAll('ul.menu_active').length; j++) {
-            document.querySelectorAll('ul.menu_active').item(j).classList.remove('menu_active');
+    	if ( menu__item.item(i).querySelector('ul').classList.contains('menu_active') == true){
+        	menu__item.item(i).querySelector('ul').classList.remove('menu_active');
         }
-        menu__item.item(i).querySelector('ul').classList.toggle('menu_active');  
+        else {
+        	for (let j = 0; j < document.querySelectorAll('ul.menu_active').length; j++) {
+            	document.querySelectorAll('ul.menu_active').item(j).classList.remove('menu_active');
+        	}
+        	menu__item.item(i).querySelector('ul').classList.add('menu_active');  
+        }
         return false;
     };
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,14 +3,30 @@ const right = document.getElementsByClassName("slider__arrow_next");
 const active = document.getElementsByClassName("slider__item");
 let i = 0;
 left[0].onclick = () => {
+	dot[i].className = "slider__dot";
 	active[i].className = "slider__item";
-	i == 0 ? i = 5 : i;
-	active[i--].className = "slider__item slider__item_active";
-	i--;
+	i == 0 ? i = active.length : i;
+	active[--i].className = "slider__item slider__item_active";
+	dot[i].className = "slider__dot slider__dot_active";
 }
 right[0].onclick = () => {
 	active[i].className = "slider__item";
-	i == 4 ? i = -1 : i;
-	active[i++].className = "slider__item slider__item_active";
-	i++;
+	dot[i].className = "slider__dot";
+	i == active.length - 1 ? i = -1 : i;
+	active[++i].className = "slider__item slider__item_active";
+	dot[i].className = "slider__dot slider__dot_active";
 }
+
+
+const dot = document.getElementsByClassName("slider__dot");
+for (let j = 0; j < dot.length; j++){
+	dot.item(j).onclick = () => {
+		dot[i].className = "slider__dot";
+		active[i].className = "slider__item";
+		dot[j].className = "slider__dot slider__dot_active";
+		active[j].className = "slider__item slider__item_active";
+		i = j;
+		return false;
+	}
+}
+
